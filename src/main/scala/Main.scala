c45c8bbe48f24e8401608ecf521ae39e1f1273c3
shuchen <liushuchen30@foxmail.com>
2023-04-05 22:50:48 +0800

    feat: init

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,12 @@
+import com.github.luben.zstd._
+
 object Main {
   def main(args: Array[String]): Unit = {
-    println("Hello world!")
+
+    val input        = "Hello World".getBytes("UTF-8")
+    val compressed   = Zstd.compress(input)
+    val decompressed = Zstd.decompress(compressed, 10)
+
+    println(new String(decompressed, "UTF-8"))
   }
 }
